957de0c4e6da06692c847604cb99cb4600f8c04e
Joe Enan <33413616+JoeEnan@users.noreply.github.com>
2025-04-07 13:43:10 +0800

    update name changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f "docker-compose.traefik.yml" ]; then
-    docker-compose -f docker-compose.yml -f docker-compose.traefik.yml up "$@"
+    docker compose -f docker-compose.yml -f docker-compose.traefik.yml up "$@"
 else
-    docker-compose -f docker-compose.yml up "$@"
+    docker compose -f docker-compose.yml up "$@"
 fi
